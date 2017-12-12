68971c782a4f77390a5abe8147e56f93f09b89f5
Luke <luke@luke.co.pl>
2017-12-12 13:13:48 +0100

    Add includes functionality

--- compile.py
+++ compile.py
@@ -5,20 +5,40 @@ import tempfile
 import os
 import subprocess
 
-code = subprocess.check_output(['uglifyjs', '--compress', '--mangle', 'toplevel', 'main.js']).decode('utf-8')
+with open('main.js') as f:
+    code = f.read()
+
+
+def replace_include(match):
+    with open('js_includes/{}'.format(match.group(1))) as f:
+        contents = f.read()
+    return contents
+
+
+while True:
+    code, n = re.subn(r'//[ ]*include{([a-z0-9/\.-_]+)}', replace_include, code)
+    if n == 0:
+        break
+
 
 def replace_base64(match):
     with open(match.group(1), 'rb') as f:
         contents = f.read()
     return base64.b64encode(contents).decode('utf-8')
 
+
 def replace_datauri(match):
     with open(match.group(2), 'rb') as f:
         contents = f.read()
     return 'data:{};base64,{}'.format(match.group(1), base64.b64encode(contents).decode('utf-8'))
 
+
 code = re.sub(r'#base64{([a-z0-9/\.-_]+)}', replace_base64, code)
 code = re.sub(r'#datauri{([a-z/]+),([a-z0-9/\.-_]+)}', replace_datauri, code)
+
+with tempfile.NamedTemporaryFile('w') as f:
+    f.write(code)
+    code = subprocess.check_output(['uglifyjs', '--compress', '--mangle', 'toplevel', f.name]).decode('utf-8')
 
 with tempfile.NamedTemporaryFile('w') as f:
     f.write(code)
--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-let audioCtx = new AudioContext(),
-    source = audioCtx.createBufferSource(),
-    byteString = atob('#base64{assets/sounds/background.ogg}'),
-    ia = new Uint8Array(byteString.length);
-
-for (let i = 0; i < byteString.length; ++i) {
-    ia[i] = byteString.charCodeAt(i);
-}
-
-audioCtx.decodeAudioData(ia.buffer, function(buffer) {
-   source.buffer = buffer;
-   source.connect(audioCtx.destination);
-   source.loop = true
-   source.start(0);
-});
+// include{background_music.js}
 
 document.body.innerHTML = ''
