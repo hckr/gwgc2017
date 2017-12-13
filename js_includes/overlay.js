a0ed7f095792e6348a600915cfea516a3c007cb4
Luke <luke@luke.co.pl>
2017-12-13 20:07:56 +0100

    Add opacity transition to overlay

--- js_includes/overlay.js
+++ js_includes/overlay.js
@@ -1,9 +1,17 @@
 q('m[n]').onclick = _ => hideOverlay();
 
 function showOverlay() {
+    o.style.display = 'block';
     W.classList.add('o');
 }
 
 function hideOverlay() {
     W.classList.remove('o');
 }
+
+o.addEventListener('transitionend', _ => {
+    console.log('aaa');
+    if (!W.classList.contains('o')) {
+        o.style.display = 'none';
+    }
+});
